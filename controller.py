5deb0c31bf7a488c48593e61759327b823169d93
Dmitrii Mironov <dimamironov@mail.ru>
2022-12-19 00:37:17 +0300

    Few tasks completed

--- controller.py
+++ controller.py
@@ -1,34 +1,72 @@
-import view
-import model
-import logger
+# Контроллер
 
 # Реализовать калькулятор с системой логирования:
 # 1) решение вводимых примеров (2+3) -> 5 ; 2**3 + (3+6)/(1+2) ->  11
 # 2) решение уравнений (x+3 = 0) -> -3
 # 3) упрощение многочленов (x*2 + 3*x2 + 4) -> 4*x*2 + 4
-
 # Записать в файл "задачу" от пользователя и ответ.
 
+import os
+import view
+import model
+import logger
 
-def run_calc():
+
+def run_calc() -> int:
+
+    """Функция реализует взаимодействие пользователя с данными."""
+
+    os.system('cls')
+
     mode = view.choose()
+    
     if mode == '1':
+        
         expr = view.get_expr()
+        
         result = model.execute_expr(expr)
+        
         view.show_res(result)
+        
         logger.log_exec(expr, result)
+        
+        input('Нажмите Enter, чтобы продолжить ...')
+        
+        run_calc()
+
     elif mode == '2':
+        
         expr = view.get_expr()
+        
         result = model.solve_eq(expr)
+        
         view.show_res(result)
+        
         logger.log_exec(expr, result)
+
+        input('Нажмите Enter, чтобы продолжить ...')
+        
+        run_calc()
+    
     elif mode == '3':
         expr = view.get_expr()
         result = model.simpify_pol(expr)
         view.show_res(result)
         logger.log_exec(expr, result)
+    
     elif mode == '4':
+        
         history = logger.get_history()
+        
         view.show_history(history)
+    
+    elif mode == '5':
+        exit
+
     else:
+
         view.erorr_mode()
+        
+        input('Нажмите Enter, чтобы продолжить ...')
+        
+        run_calc()
--- logger.py
+++ logger.py
@@ -1,11 +1,28 @@
 # Модуль для записи резуьтатов вычислений
 
+file_name = 'log.txt'
+
 def log_exec(expr: str, result: str):
-    """Записывает в файл результат вычислений
-    в виде |задача -> ответ|"""
-    pass
+    
+    """Записывает в файл результат вычислений в виде |задача -> ответ|."""
+    
+    log = ('[' + expr + ' -> ' + result + ']\n')
+
+    with open('log.txt', 'a', encoding = 'utf-8') as f:
+        f.writelines(log)
 
 
 def get_history() -> str:
-    """"Возвращает содержимое файла с результатами вычислений"""
-    pass
+    
+    """"Возвращает содержимое файла с результатами вычислений."""
+    
+    with open('log.txt', 'r', encoding='utf-8') as f:
+        file_contents = f.read()
+
+        if len(file_contents) == 0:
+            
+            print('Список логов пуст!')
+        
+        else:
+            
+            return file_contents
